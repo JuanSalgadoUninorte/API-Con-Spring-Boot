6d14e08906b790289c157cd80a29f6d8f3d26778
Juan David Salgado <90982336+JuanSalgadoUninorte@users.noreply.github.com>
2023-04-13 12:36:24 -0500

    Add files via upload

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String helloWorld() {
-        return "Hello titu pacalapihu world";
+    public String helloWorld(){
+        return  "Hello dsfghklfkf World";
     }
 }
